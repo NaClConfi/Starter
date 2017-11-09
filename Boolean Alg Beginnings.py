06498b8bea63b2a773de49cf22e0a4169c7d3ab4
NaClConfi <32175604+NaClConfi@users.noreply.github.com>
2017-11-08 22:43:58 -0500

    Add files via upload

---
+++ Boolean Alg Beginnings.py
@@ -0,0 +1,72 @@
+#Boolean Algebra
+
+#if CONDITION ---> True/False THEN THIS
+
+#if True:
+    #print('Sup')
+
+#if False:
+   # print('NaCl Confi is whack')
+
+# Comparison Operators: == equality verification, < gt, > lt, = equal, >= less than or equal to, <= greater than or equal to, != not equal to
+
+6 == 6
+
+Nickshours = 40
+
+#Nickshours != 99934
+
+#if Nickshours > 39:
+   # print('Make him manager')
+
+#Oatmealtemp_f = 200
+
+#if Oatmealtemp_f < 215:
+  #  print('microwave it!!!')
+
+#phonebattery_percent = 15
+
+#if phonebattery_percent < 2:
+#    print("Charge your phone clown")
+
+#Ebons_in_circulation = 21030000
+
+#numbers = list(range(15))
+
+#print(numbers)
+
+#if Ebons_in_circulation < 500000000:
+        #numbers.append(-555)
+
+
+
+
+
+#my_popped_tires = False
+#eggplant_lasagne = False
+
+#True and True
+
+#if True:
+ #   print(list(range(6)))
+
+#NaCl_Confi_Barz = 40
+
+#if NaCl_Confi_Barz > 33:
+  #  print('He wins the battle!!')
+
+#Nickstudytime = True
+#Nickbarzprepared = True
+
+#if Nickstudytime and Nickbarzprepared:
+ #   print('success!!!')
+
+if True:
+    print('time to write')
+
+    
+
+    
+
+
+    
